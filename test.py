b4693a1483e7478b5073568bbbb30578e9789e5d
Vincenzo Pio Florio <vincenzopioflorio@gmail.com>
2024-02-20 10:51:45 +0100

    Improved communication

--- test.py
+++ test.py
@@ -11,13 +11,18 @@ ser = serial.Serial('COM14', 250000)
 # Invia il comando di start al microcontrollore
 time.sleep(1)
 ser.write('s'.encode())
-time.sleep(1)
+time.sleep(3)
 
 # Aspetta che il microcontrollore mandi modalità corrente e data rate
 while True:
     if ser.inWaiting():
-        str = ser.readline().decode().strip()
-        print("Stringa ricevuta: " + str)
+        received_msg = ser.readline().decode().strip()  # Legge il valore s dalla seriale (HEX) 0x73
+        current_mode = ser.readline().decode().strip()
+        print("Current mode: " + current_mode)
+        data_rate = int(ser.readline().decode().strip())
+        print("Data rate: ", data_rate)
+
+        print("\nStarting data acquisition...")
         break
 
 
@@ -34,13 +39,14 @@ try:
                 low_byte = ser.read(1)  # Leggi il low byte
                 measurement = (ord(high_byte) << 8) | ord(low_byte)  # Unisci i byte
                 data_array.append(measurement)  # Aggiungi la misurazione all'array
-                if len(data_array) == 860:  # Se l'array ha raggiunto la lunghezza desiderata
+                if len(data_array) == data_rate:  # Se l'array ha raggiunto la lunghezza desiderata
                     data_matrix.append(data_array)  # Aggiungi l'array alla matrice
                     data_array = []  # Resetta l'array
 except KeyboardInterrupt:
     # Quando il programma viene interrotto, salva la matrice in un file di testo
     data_matrix = data_matrix[1:]  # Rimuovi la prima riga (incompleta)
-    np.savetxt('data_matrix.txt', data_matrix)
+    utils = 'Current mode: ' + current_mode + '\nData rate: ' + str(data_rate) + ' Hz\n\n'
+    np.savetxt('data_matrix.txt', data_matrix, header=utils, fmt='%d')
     print("Dati salvati in 'data_matrix.txt'")
 finally:
     ser.close()  # Chiudi la connessione seriale
@@ -48,11 +54,11 @@ finally:
     # Appiattisci la matrice in un array unidimensionale
     data_array = np.concatenate(data_matrix)
 
-    # Crea un array di tempi. Ogni 860 campioni corrispondono a un secondo.
-    time_array = np.arange(len(data_array)) / 860
+    # Crea un array di tempi. Ogni data-rate campioni corrispondono a un secondo.
+    time_array = np.arange(len(data_array)) / data_rate
 
     # Crea un grafico
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(10, 6), dpi=500)
     plt.plot(time_array, data_array)
     plt.title('Grafico dei dati')
     plt.xlabel('Tempo (s)')
