e8b67864614a873229cdb36c0fcf41ad6cd14f44
dav01luffy <davidet952@gmail.com>
2024-02-24 15:58:47 +0100

    add k value + fix
    - version without fake last timestamp

--- plotData.py
+++ plotData.py
@@ -6,35 +6,39 @@ from collections import deque
 
 
 #export the current mode
-currentmode=np.loadtxt('data_matrix.txt', dtype='str', max_rows=1)[-1] 
+currentmode=np.loadtxt('dataStorage.txt', dtype='str', max_rows=1)[-1]
 
 #export the data rate
-data_rate=int(np.loadtxt('data_matrix.txt', dtype='int', usecols=(2), skiprows=1,max_rows=1))
+data_rate=int(np.loadtxt('dataStorage.txt', dtype='int', usecols=(2), skiprows=1,max_rows=1))
 
+#export the k value
+k_value=float(np.loadtxt('dataStorage.txt', dtype='float', usecols=(2), skiprows=2,max_rows=1))
 
 #export the offset
-offset=np.loadtxt('data_matrix.txt', dtype='float', usecols=(2), skiprows=2, max_rows=1) 
+offset=np.loadtxt('dataStorage.txt', dtype='float', usecols=(2), skiprows=3, max_rows=1) 
 
 #export the timestamp
-timestamp = np.loadtxt('data_matrix.txt', dtype='str', usecols=(0), skiprows=3) 
+timestamp = np.loadtxt('dataStorage.txt', dtype='str', usecols=(0), skiprows=4) 
+
 
 #export the data matrix
-data_matrix = np.loadtxt('data_matrix.txt', dtype='int', usecols=np.arange(1, data_rate+1), skiprows=3) 
-
+data_matrix = np.loadtxt('dataStorage.txt', dtype='int', skiprows=4, usecols=np.arange(1, data_rate+1),max_rows=len(timestamp)-1)
 
 #create a 1D array from the matrix
 data_array = np.concatenate(data_matrix) 
 
+#declaration of the mean values array
+mean_values = np.zeros(data_matrix.shape[0]) 
 
-mean_values = np.zeros(data_matrix.shape[0]) #declaration of the mean values array
-std_values = np.zeros(data_matrix.shape[0]) #declaration of the standard deviation values array
+#declaration of the standard deviation values array
+std_values = np.zeros(data_matrix.shape[0]) 
 
 # Calculate the mean and standard deviation for each row
 for i in range(0, data_matrix.shape[0]):
     mean_values[i] = np.mean(data_matrix[i,:])
     std_values[i]= np.std(data_matrix[i,:])
 
-
+"""
 #declarations for the dynamic plot
 fig, ax = plt.subplots() 
 line, = ax.plot([]) 
@@ -73,7 +77,7 @@ for i in range(1, len(timestamp)):
         new_x = timeline[j]
 
         # add the corresponding value to new_y
-        new_y = data_matrix[i,j]
+        new_y = data_matrix[i-1,j]
 
         #list of tuples
         data_points.append((new_x, new_y)) 
@@ -84,65 +88,34 @@ for i in range(1, len(timestamp)):
         line.set_data(x_values, y_values) 
 
         #pause the plot for a short time
-        plt.pause(0.0001) 
+        plt.pause(0.1) 
   
 
-##create a fake timestamp for the last plot with delta time of 1 second        
-# ~Start Last plot        
-faketimestamp=timedelta(seconds=1)
-finaltimestamp=datetime.strptime(timestamp[-1], '%H:%M:%S')
-finaltimeline=np.arange(finaltimestamp, finaltimestamp + faketimestamp, faketimestamp/data_rate)
-     
-if(timeline.shape>(data_rate,)):
-        timeline=np.delete(timeline, -1)
-        
-#set the limits of the x axis
-ax.set_xlim(finaltimestamp, (finaltimestamp + faketimestamp)) 
-for j in range(0, data_rate): 
-
-    # add every single value in the timeline to new_x
-    new_x = finaltimeline[j]
-
-    # add the corresponding value to new_y
-    new_y = data_matrix[-1,j]
-
-    #list of tuples
-    data_points.append((new_x, new_y)) 
-  
-    # Update the plot with the new data points 
-    x_values = [x for x, y in data_points] 
-    y_values = [y for x, y in data_points] 
-    line.set_data(x_values, y_values) 
-
-    #pause the plot for a short time
-    plt.pause(0.0001) 
-
-
-# ~End
-    
 #clear   
 line.set_data([], [])
 
 #show all
 plt.grid()
 plt.show() 
-
+"""
 
 # Create the mean graphs
 plt.figure(figsize=(10, 5), dpi=100)
-plt.plot(timestamp, mean_values)
+plt.plot(timestamp[:(len(timestamp)-1)], mean_values)
 plt.title('Mean value over time')
 plt.xlabel('Time (s)')
 plt.ylabel('Mean value')
 plt.grid()
+plt.show()
 
 
 #Create the standard deviation graph with error bands
 plt.figure(figsize=(10, 5), dpi=100)
 
 #plt.subplot(2, 1, 2)
-plt.plot(timestamp, std_values)
-plt.fill_between(timestamp,
+plt.plot(timestamp[:(len(timestamp)-1)], std_values)
+
+plt.fill_between(timestamp[:(len(timestamp)-1)],
                  std_values - stats.t.ppf(0.975, df=data_rate - 1) * std_values / np.sqrt(data_rate),
                  std_values + stats.t.ppf(0.975, df=data_rate - 1) * std_values / np.sqrt(data_rate), color='gray',
                  alpha=0.5)
