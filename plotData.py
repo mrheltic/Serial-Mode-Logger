533da12421f4fb4dc6f9a8b7a35f2e11964a0614
dav01luffy <davidet952@gmail.com>
2024-03-08 18:53:04 +0100

    fix the way to delete arrays in the dataset

--- plotData.py
+++ plotData.py
@@ -5,7 +5,8 @@ import scipy.stats as stats
 from collections import deque 
 from matplotlib.animation import FuncAnimation 
 
-
+#number of array to skip in the dataset
+number_of_rows_to_skip = 1
 
 #export the current mode
 currentmode=np.loadtxt('dataStorage.txt', dtype='str', max_rows=1)[-1]
@@ -23,13 +24,10 @@ data_rate=int(np.loadtxt('dataStorage.txt', dtype='int', usecols=(4), skiprows=3
 factor=np.loadtxt('dataStorage.txt', dtype='float', usecols=(1), skiprows=4, max_rows=1) 
 
 #export the timestamp
-timestamp = np.loadtxt('dataStorage.txt', dtype='str', usecols=(0), skiprows=6) 
+timestamp = np.loadtxt('dataStorage.txt', dtype='str', usecols=(0), skiprows=5+number_of_rows_to_skip) 
 
 #export the dataset(reversed) without the 1st array for a problem
-data_matrix = np.loadtxt('dataStorage.txt', dtype='int', skiprows=6, usecols=np.arange(1, data_rate+1),max_rows=len(timestamp)-1)
-
-# Remove the first column from matrix (various errors)
-data_matrix = data_matrix[1:]
+data_matrix = np.loadtxt('dataStorage.txt', dtype='int', skiprows=5+number_of_rows_to_skip, usecols=np.arange(1, data_rate+1),max_rows=len(timestamp)-1)
 
 #create a 1D array from the matrix
 data_array = np.concatenate(data_matrix) # It could be changed to .reshape
